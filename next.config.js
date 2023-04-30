fc7d47a6834c6fb7d8d1c49ce588d32e962ed1a4
Jahanzaib Tayyab <jahanzaib.tayyab@devigital.com>
2023-04-30 17:34:50 +0500

    api development

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  env: {
+    PGHOST: "ep-red-sunset-577822.us-east-2.aws.neon.tech",
+    PGDATABASE: "neondb",
+    PGUSER: "zahid-frooq",
+    PGPASSWORD: "j6xyKP2XQmUA",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
